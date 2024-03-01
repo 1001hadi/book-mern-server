6762cae4bf6c55467da8c267926e5b5dfb9a6a1b
1001hadi <hadi.jaf.1001@gmail.com>
2024-02-29 18:43:38 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+// handdle cors middleware
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MERN!");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to Mongo");
+    app.listen(PORT, () => {
+      console.log(`App is listening to porth:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
